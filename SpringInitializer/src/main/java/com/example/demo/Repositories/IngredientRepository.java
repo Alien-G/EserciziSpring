776868f2087d9733f259d40a0c8f050fcce0d42b
GA <gaia.addeo@gmail.com>
2023-07-30 16:46:19 +0200

    Exercise 1.

--- SpringInitializer/src/main/java/com/example/demo/Repositories/IngredientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Repositories;
-
-import com.example.demo.Entities.Ingredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
-}
